84a36635a023dff77393d5b524c75a265a1c16ce
Adam <admosity@users.noreply.github.com>
2018-08-24 14:14:39 -0400

    fix: make `name` in InjectModel optional

--- lib/common/mongoose.decorators.ts
+++ lib/common/mongoose.decorators.ts
@@ -4,5 +4,5 @@ import { getConnectionToken, getModelToken } from './mongoose.utils';
 
 export const InjectModel = (model: string) => Inject(getModelToken(model));
 
-export const InjectConnection = (name: string) =>
+export const InjectConnection = (name?: string) =>
   Inject(name ? getConnectionToken(name) : DEFAULT_DB_CONNECTION);
