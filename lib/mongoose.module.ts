8c9712040cf9513e9ded2240e740ed772e09a61a
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-04-15 17:41:42 +0200

    bugfix(): fix optional forFeature() call

--- lib/mongoose.module.ts
+++ lib/mongoose.module.ts
@@ -4,6 +4,7 @@ import {
   MongooseModuleOptions,
 } from './interfaces/mongoose-options.interface';
 import { MongooseCoreModule } from './mongoose-core.module';
+import { DEFAULT_DB_CONNECTION } from './mongoose.constants';
 import { createMongooseProviders } from './mongoose.providers';
 
 @Module({})
@@ -27,7 +28,7 @@ export class MongooseModule {
 
   static forFeature(
     models: { name: string; schema: any; collection?: string }[] = [],
-    connectionName: string,
+    connectionName: string = DEFAULT_DB_CONNECTION,
   ): DynamicModule {
     const providers = createMongooseProviders(connectionName, models);
     return {
