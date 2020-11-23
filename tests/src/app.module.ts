766edd4f420c3ef11af59dce750e14c12d3a8994
Pong <samfunghp@gmail.com>
2020-11-23 22:31:17 +0800

    fix: make discriminator test better

--- tests/src/app.module.ts
+++ tests/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '../../lib';
 import { CatsModule } from './cats/cats.module';
-import { EventModule } from './event/event.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/test'),
     CatsModule,
-    EventModule,
   ],
 })
 export class AppModule {}
