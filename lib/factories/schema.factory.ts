d93ace5f1fe2f84abe4cb22f84ad8b63f28eeac4
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2021-01-07 10:12:07 +0100

    fix(): return a typed schema from create for class method

--- lib/factories/schema.factory.ts
+++ lib/factories/schema.factory.ts
@@ -4,12 +4,14 @@ import { TypeMetadataStorage } from '../storages/type-metadata.storage';
 import { DefinitionsFactory } from './definitions.factory';
 
 export class SchemaFactory {
-  static createForClass<T = any>(target: Type<unknown>) {
+  static createForClass<T extends Type<unknown> = any>(
+    target: T,
+  ): mongoose.Schema<mongoose.Document<InstanceType<T>>> {
     const schemaDefinition = DefinitionsFactory.createForClass(target);
     const schemaMetadata = TypeMetadataStorage.getSchemaMetadataByTarget(
       target,
     );
-    return new mongoose.Schema(
+    return new mongoose.Schema<mongoose.Document<InstanceType<T>>>(
       schemaDefinition,
       schemaMetadata && schemaMetadata.options,
     );
