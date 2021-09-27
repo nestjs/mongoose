6b0a107a105492c5dadf7864a08dde49545c578d
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2021-09-27 10:33:15 +0200

    chore(): upgrade to mongoose v6

--- lib/factories/schema.factory.ts
+++ lib/factories/schema.factory.ts
@@ -1,5 +1,6 @@
 import { Type } from '@nestjs/common';
 import * as mongoose from 'mongoose';
+import { SchemaDefinition, SchemaDefinitionType } from 'mongoose';
 import { TypeMetadataStorage } from '../storages/type-metadata.storage';
 import { DefinitionsFactory } from './definitions.factory';
 
@@ -8,14 +9,13 @@ export class SchemaFactory {
     TClass extends any = any,
     TDocument extends mongoose.Document = TClass extends mongoose.Document
       ? TClass
-      : mongoose.Document<TClass>
+      : mongoose.Document<TClass>,
   >(target: Type<TClass>): mongoose.Schema<TDocument> {
     const schemaDefinition = DefinitionsFactory.createForClass(target);
-    const schemaMetadata = TypeMetadataStorage.getSchemaMetadataByTarget(
-      target,
-    );
+    const schemaMetadata =
+      TypeMetadataStorage.getSchemaMetadataByTarget(target);
     return new mongoose.Schema<TDocument>(
-      schemaDefinition,
+      schemaDefinition as SchemaDefinition<SchemaDefinitionType<TDocument>>,
       schemaMetadata && schemaMetadata.options,
     );
   }
