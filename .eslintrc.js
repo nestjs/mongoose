099e0789784cab0d3a01bbdeb0ea095982da3208
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2021-09-27 10:37:14 +0200

    chore(): fix linter setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['./tsconfig.json', './tsconfig.build.json'],
+    project: 'tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
