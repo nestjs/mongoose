0ca9a0805110488fd6229a70a6ce8ca538f23339
codytseng <codytseng@outlook.com>
2022-10-24 00:09:45 +0800

    test: optimize `@Plugin` decorator test

--- tests/src/cats/cats.controller.ts
+++ tests/src/cats/cats.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { LeanDocument } from 'mongoose';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './schemas/cat.schema';
@@ -9,14 +8,12 @@ export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Post()
-  async create(@Body() createCatDto: CreateCatDto): Promise<LeanDocument<Cat>> {
-    const cat = await this.catsService.create(createCatDto);
-    return cat.toObject({ virtuals: true });
+  async create(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto);
   }
 
   @Get()
-  async findAll(): Promise<LeanDocument<Cat>[]> {
-    const cats = await this.catsService.findAll();
-    return cats.map((cat) => cat.toObject({ virtuals: true }));
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 }
--- tests/src/cats/schemas/cat.schema.ts
+++ tests/src/cats/schemas/cat.schema.ts
@@ -1,13 +1,22 @@
 import { Document, Schema as MongooseSchema } from 'mongoose';
 import { Prop, Schema, SchemaFactory, Plugin } from '../../../../lib';
 
-export function BirthYearPlugin(schema: MongooseSchema) {
-  schema.virtual('birthYear').get(function () {
-    return new Date().getFullYear() - (this.age || 0);
+export function BirthYearPlugin(
+  schema: MongooseSchema,
+  options?: { ageKey?: string },
+) {
+  schema.add({ birthYear: { type: Number } });
+
+  schema.pre('save', function (next) {
+    if (!this.birthYear) {
+      const ageKey = options?.ageKey ?? 'age';
+      this.birthYear = new Date().getFullYear() - (this[ageKey] || 0);
+    }
+    next();
   });
 }
 
-@Plugin(BirthYearPlugin)
+@Plugin(BirthYearPlugin, { ageKey: 'age' })
 @Schema()
 export class Cat extends Document {
   @Prop()
