a0342636cbcd2fd345c9346348d292cf3155dcc8
Shuki Kurant <shuki.kurant@cyberproof.com>
2023-11-07 15:56:01 +0200

    fix: Fix providers tokens for multiple databases

--- lib/mongoose.providers.ts
+++ lib/mongoose.providers.ts
@@ -7,30 +7,46 @@ export function createMongooseProviders(
   connectionName?: string,
   options: ModelDefinition[] = [],
 ): Provider[] {
-  return options.reduce(
-    (providers, option) => [
+  return options.reduce((providers, option) => {
+    const connectionToken = getConnectionToken(connectionName);
+    const modelToken = getModelToken(option.name, connectionName);
+
+    const modelFactory = (connection: Connection) => {
+      const model = connection.models[option.name]
+        ? connection.models[option.name]
+        : connection.model(option.name, option.schema, option.collection);
+
+      return model;
+    };
+
+    return [
       ...providers,
-      ...(option.discriminators || []).map((d) => ({
-        provide: getModelToken(d.name, connectionName),
-        useFactory: (model: Model<Document>) =>
-          model.discriminator(d.name, d.schema, d.value),
-        inject: [getModelToken(option.name, connectionName)],
-      })),
-      {
-        provide: getModelToken(option.name, connectionName),
-        useFactory: (connection: Connection) => {
-          const model = connection.models[option.name] ? connection.models[option.name] : connection.model(
-            option.name,
-            option.schema,
-            option.collection,
-          );
-          return model;
+      ...(option.discriminators || []).flatMap((d) => [
+        {
+          provide: getModelToken(d.name),
+          useFactory: (model: Model<Document>) =>
+            model.discriminator(d.name, d.schema, d.value),
+          inject: [modelToken],
         },
-        inject: [getConnectionToken(connectionName)],
+        {
+          provide: getModelToken(d.name, connectionName),
+          useFactory: (model: Model<Document>) =>
+            model.discriminator(d.name, d.schema, d.value),
+          inject: [modelToken],
+        },
+      ]),
+      {
+        provide: getModelToken(option.name),
+        useFactory: modelFactory,
+        inject: [connectionToken],
       },
-    ],
-    [] as Provider[],
-  );
+      {
+        provide: modelToken,
+        useFactory: modelFactory,
+        inject: [connectionToken],
+      },
+    ];
+  }, [] as Provider[]);
 }
 
 export function createMongooseAsyncProviders(
@@ -38,27 +54,41 @@ export function createMongooseAsyncProviders(
   modelFactories: AsyncModelFactory[] = [],
 ): Provider[] {
   return modelFactories.reduce((providers, option) => {
+    const connectionToken = getConnectionToken(connectionName);
+    const modelToken = getModelToken(option.name, connectionName);
+
+    const modelFactory = async (connection: Connection, ...args: unknown[]) => {
+      const schema = await option.useFactory(...args);
+      const model = connection.model(option.name, schema, option.collection);
+      return model;
+    };
+
     return [
       ...providers,
       {
-        provide: getModelToken(option.name, connectionName),
-        useFactory: async (connection: Connection, ...args: unknown[]) => {
-          const schema = await option.useFactory(...args);
-          const model = connection.model(
-            option.name,
-            schema,
-            option.collection,
-          );
-          return model;
-        },
-        inject: [getConnectionToken(connectionName), ...(option.inject || [])],
+        provide: modelToken,
+        useFactory: modelFactory,
+        inject: [connectionToken, ...(option.inject || [])],
       },
-      ...(option.discriminators || []).map((d) => ({
-        provide: getModelToken(d.name, connectionName),
-        useFactory: (model: Model<Document>) =>
-          model.discriminator(d.name, d.schema, d.value),
-        inject: [getModelToken(option.name, connectionName)],
-      })),
+      {
+        provide: getModelToken(option.name),
+        useFactory: modelFactory,
+        inject: [connectionToken, ...(option.inject || [])],
+      },
+      ...(option.discriminators || []).flatMap((d) => [
+        {
+          provide: getModelToken(d.name, connectionName),
+          useFactory: (model: Model<Document>) =>
+            model.discriminator(d.name, d.schema, d.value),
+          inject: [modelToken],
+        },
+        {
+          provide: getModelToken(d.name),
+          useFactory: (model: Model<Document>) =>
+            model.discriminator(d.name, d.schema, d.value),
+          inject: [modelToken],
+        },
+      ]),
     ];
   }, [] as Provider[]);
 }
